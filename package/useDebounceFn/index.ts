04bc84983ffa104e474440b069b1d5e488d21404
Peter.Zheng <winchaozheng@gmail.com>
2024-06-21 15:46:43 +0800

    Create index.ts

---
+++ package/useDebounceFn/index.ts
@@ -0,0 +1,38 @@
+import { useMemo } from 'react';
+import debounce from '../lodash/debounce.ts';
+import type { DebounceOptions } from '../useDebounce/debounceOptions';
+import useLatest from '../useLatest';
+import useUnmount from '../useUnmount';
+import { isFunction } from '../utils';
+
+type noop = (...args: any[]) => any;
+
+function useDebounceFn<T extends noop>(fn: T, options?: DebounceOptions) {
+  const fnRef = useLatest(fn);
+
+  const wait = options?.wait ?? 1000;
+
+  const debounced = useMemo(
+    () =>
+      debounce(
+        (...args: Parameters<T>): ReturnType<T> => {
+          return fnRef.current(...args);
+        },
+        wait,
+        options,
+      ),
+    [],
+  );
+
+  useUnmount(() => {
+    debounced.cancel();
+  });
+
+  return {
+    run: debounced,
+    cancel: debounced.cancel,
+    flush: debounced.flush,
+  };
+}
+
+export default useDebounceFn;
